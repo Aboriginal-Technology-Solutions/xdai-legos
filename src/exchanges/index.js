790c4fa513604f0b507ae472e5fe554222a41824
Krypto Kajun <kryptokajun@protonmail.com>
2021-05-01 00:18:08 +0000

    First Commit

---
+++ src/exchanges/index.js
@@ -0,0 +1,11 @@
+const honeyswap = require('honeyswap')
+const sushiswap = require('sushiswap')
+const baoswap = require('baoswap')
+const levinswap = require('levinswap')
+
+module.exports = {
+  honeyswap,
+  levinswap,
+  baoswap,
+  sushiswap
+}
